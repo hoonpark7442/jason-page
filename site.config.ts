31c7cec5f28934ecc5b4520a474c4c5485b6a129
hoonpark <hoon@whattime.co.kr>
2024-09-21 14:11:07 +0900

    first commit

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '101cc9815b6f8088bee4d44201d385df',
+  rootNotionPageId: '108cc9815b6f8084970bf792ff14234b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -57,7 +57,7 @@ export default siteConfig({
   navigationLinks: [
     {
       title: '운동',
-      pageId: 'f58359830f804d7dbcc65256e9adaa11'
+      pageId: 'fffcc9815b6f81f0913dca1ef7373375'
     },
   ]
 })
